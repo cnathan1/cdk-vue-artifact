a91064520eadc157604ddb854cbbfc51cd09c3e7
butlercw <butlercw@amazon.com>
2021-07-02 14:09:38 -0400

    Fix CDK stack

---
+++ cdk/bin/cdk-vue-artifact.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { CdkVueArtifactStack } from '../lib/cdk-vue-artifact-stack';
+
+const app = new cdk.App();
+new CdkVueArtifactStack(app, 'CdkVueArtifactStack');
