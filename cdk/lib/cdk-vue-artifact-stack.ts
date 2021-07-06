81aab63badd72df3c85a83e29082cc44902a01f1
butlercw <butlercw@amazon.com>
2021-07-06 17:36:39 -0400

    Add separate build for Vue vs. CDK

--- cdk/lib/cdk-vue-artifact-stack.ts
+++ cdk/lib/cdk-vue-artifact-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as core from '@aws-cdk/core';
-import {CfnParameter} from '@aws-cdk/core';
+import {CfnParameter, Construct, Stage, StageProps} from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as pipeline from '@aws-cdk/aws-codepipeline'
 import * as codebuild from '@aws-cdk/aws-codebuild'
 import * as sns from '@aws-cdk/aws-sns'
 import * as pipelineActions from '@aws-cdk/aws-codepipeline-actions'
 import * as sm from "@aws-cdk/aws-secretsmanager";
-import {CdkPipeline, SimpleSynthAction} from '@aws-cdk/pipelines';
+import {CdkPipeline, CdkStage, CdkStageProps, SimpleSynthAction} from '@aws-cdk/pipelines';
 import {Distribution, LambdaEdgeEventType, OriginAccessIdentity} from '@aws-cdk/aws-cloudfront';
 import {S3Origin} from '@aws-cdk/aws-cloudfront-origins';
 import {EdgeFunction} from "@aws-cdk/aws-cloudfront/lib/experimental";
@@ -39,6 +39,95 @@ export class CdkVueArtifactStack extends cdk.Stack {
             description: ''
         });
 
+        const pipelineArtifact = new pipeline.Artifact('RepoSource');
+        const buildArtifact = new pipeline.Artifact('BuildOutput');
+        const vueBuildArtifact = new pipeline.Artifact('VueBuildOutput');
+        const pipelineNotificationTopic = new sns.Topic(this, 'ApprovalSnsTopic', {
+            topicName: 'ApprovalSnsTopic'
+        });
+        const cdkPipeline = new CdkPipeline(this, 'VueComponentCdkPipeline', {
+            pipelineName: 'VueComponentPipeline',
+            cloudAssemblyArtifact: pipelineArtifact,
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact: pipelineArtifact,
+                cloudAssemblyArtifact: buildArtifact,
+                subdirectory: 'cdk',
+                buildCommand: 'npm run build',
+                testCommands: [
+                    'npm run test'
+                ]
+            }),
+            sourceAction: new pipelineActions.GitHubSourceAction({
+                actionName: 'GitHubSource',
+                branch: githubBranch.valueAsString,
+                output: pipelineArtifact,
+                owner: githubOwner.valueAsString,
+                repo: githubRepoName.valueAsString,
+                oauthToken: sm.Secret.fromSecretNameV2(this, 'MyGithubToken', 'my-github-token')
+                    .secretValueFromJson('my-github-token'),
+                trigger: pipelineActions.GitHubTrigger.WEBHOOK,
+                runOrder: 1
+            })
+        });
+
+
+        const vueApplicationStage = cdkPipeline.addStage('BuildVue');
+        vueApplicationStage.addActions(new pipelineActions.CodeBuildAction({
+            actionName: 'BuildVue',
+            input: pipelineArtifact,
+            outputs: [
+                vueBuildArtifact
+            ],
+            project: new codebuild.PipelineProject(this, "BuildVue", {
+                buildSpec: codebuild.BuildSpec.fromObject({
+                    version: "0.2",
+                    phases: {
+                        install: {
+                            "runtime-versions": {
+                                nodejs: 14
+                            }
+                        },
+                        "pre_build": {
+                            commands: [
+                                "cd vue-web-component-app",
+                                "npm install"
+                            ]
+                        },
+                        build: {
+                            commands: [
+                                "npm run lint",
+                                "npm run test",
+                                "npm run build",
+                                "cp public/* dist"
+                            ]
+                        }
+                    },
+                    artifacts: {
+                        files: [
+                            "**/*"
+                        ],
+                        "base-directory": "vue-web-component-app/dist"
+                    }
+                }),
+                environment: {
+                    buildImage: codebuild.LinuxBuildImage.STANDARD_4_0
+                }
+            }),
+            runOrder: 1
+        }));
+
+        const approvalStage = cdkPipeline.addStage('ApprovalStage');
+        approvalStage.addActions(new pipelineActions.ManualApprovalAction({
+            actionName: 'ApproveDeploy',
+            notifyEmails: [
+                emailNotifications.valueAsString
+            ],
+            additionalInformation: 'Approve Deployment to S3?',
+            externalEntityLink: `https://github.com/${githubOwner.valueAsString}/${githubRepoName.valueAsString}`,
+            notificationTopic: pipelineNotificationTopic,
+            runOrder: 1
+        }));
+
         // Creating S3 bucket with name vue-component-bucket
         const deployBucket = new s3.Bucket(this, 'VueComponentsBucket', {
             versioned: false,
@@ -69,189 +158,12 @@ export class CdkVueArtifactStack extends cdk.Stack {
             defaultRootObject: 'index.html'
         });
 
-        // const pipelineArtifact = new pipeline.Artifact('RepoSource');
-        // const buildArtifact = new pipeline.Artifact('BuildOutput');
-        // const testBuild = new codebuild.PipelineProject(this, "TestBuild", {
-        //     buildSpec: codebuild.BuildSpec.fromObject({
-        //         version: "0.2",
-        //         phases: {
-        //             install: {
-        //                 "runtime-versions": {
-        //                     nodejs: 10
-        //                 }
-        //             },
-        //             build: {
-        //                 commands: [
-        //                     "cd vue-web-component-app",
-        //                     "npm install",
-        //                     "npm run lint",
-        //                     "npm run test"
-        //                 ]
-        //             }
-        //         }
-        //     }),
-        //     environment: {
-        //         buildImage: codebuild.LinuxBuildImage.STANDARD_4_0
-        //     }
-        // });
-        // const runBuild = new codebuild.PipelineProject(this, "RunBuild", {
-        //     buildSpec: codebuild.BuildSpec.fromObject({
-        //         version: "0.2",
-        //         phases: {
-        //             install: {
-        //                 "runtime-versions": {
-        //                     nodejs: 10
-        //                 }
-        //             },
-        //             build: {
-        //                 commands: [
-        //                     "cd vue-web-component-app",
-        //                     "npm install",
-        //                     "./node_modules/.bin/vue-cli-service build --target wc --inline-vue --name counter-app src/CounterApp.vue",
-        //                     "mv dist ../dist",
-        //                     "./node_modules/.bin/vue-cli-service build --target wc --inline-vue --name addition-app src/AdditionApp.vue",
-        //                     "cp -a dist/. ../dist/",
-        //                     "cp -a public/. ../dist/"
-        //                 ]
-        //             }
-        //         },
-        //         artifacts: {
-        //             files: [
-        //                 "**/*"
-        //             ],
-        //             "discard-paths": "no",
-        //             "base-directory": "dist"
-        //         }
-        //     }),
-        //     environment: {
-        //         buildImage: codebuild.LinuxBuildImage.STANDARD_4_0
-        //     }
-        // });
-        //
-        // const pipelineNotificationTopic = new sns.Topic(this, 'ApprovalSnsTopic', {
-        //     topicName: 'ApprovalSnsTopic'
-        // });
-        //
-        // const vuePipeline = new pipeline.Pipeline(this, 'VueComponentPipeline', {
-        //     pipelineName: 'VueComponentPipeline',
-        //     stages: [
-        //         {
-        //             stageName: 'SourceStage',
-        //             actions: [
-        //                 new pipelineActions.GitHubSourceAction({
-        //                     actionName: 'GitHubSource',
-        //                     branch: githubBranch.valueAsString,
-        //                     output: pipelineArtifact,
-        //                     owner: githubOwner.valueAsString,
-        //                     repo: githubRepoName.valueAsString,
-        //                     oauthToken: sm.Secret.fromSecretName(this, 'MyGithubToken', 'my-github-token')
-        //                         .secretValueFromJson('my-github-token'),
-        //                     trigger: pipelineActions.GitHubTrigger.WEBHOOK,
-        //                     runOrder: 1
-        //                 })
-        //             ]
-        //         },
-        //         {
-        //             stageName: 'TestStage',
-        //             actions: [
-        //                 new pipelineActions.CodeBuildAction({
-        //                     actionName: 'TestVue',
-        //                     input: pipelineArtifact,
-        //                     project: testBuild,
-        //                     runOrder: 1
-        //                 })
-        //             ]
-        //         },
-        //         {
-        //             stageName: 'BuildStage',
-        //             actions: [
-        //                 new pipelineActions.CodeBuildAction({
-        //                     actionName: 'BuildVue',
-        //                     input: pipelineArtifact,
-        //                     outputs: [
-        //                         buildArtifact
-        //                     ],
-        //                     project: runBuild,
-        //                     runOrder: 1
-        //                 })
-        //             ]
-        //         },
-        //         {
-        //             stageName: 'ApprovalStage',
-        //             actions: [
-        //                 new pipelineActions.ManualApprovalAction({
-        //                     actionName: 'ApproveDeploy',
-        //                     notifyEmails: [
-        //                         emailNotifications.valueAsString
-        //                     ],
-        //                     additionalInformation: 'Approve Deployment to S3?',
-        //                     externalEntityLink: `https://github.com/${githubOwner.valueAsString}/${githubRepoName.valueAsString}`,
-        //                     notificationTopic: pipelineNotificationTopic,
-        //                     runOrder: 1
-        //                 })
-        //             ]
-        //         },
-        //         {
-        //             stageName: 'DeployStage',
-        //             actions: [
-        //                 new pipelineActions.S3DeployAction({
-        //                     actionName: 'DeployVue',
-        //                     bucket: deployBucket,
-        //                     input: buildArtifact
-        //                 })
-        //             ]
-        //         }
-        //     ]
-        // });
-
-        const pipelineArtifact = new pipeline.Artifact('RepoSource');
-        const buildArtifact = new pipeline.Artifact('BuildOutput');
-        const pipelineNotificationTopic = new sns.Topic(this, 'ApprovalSnsTopic', {
-            topicName: 'ApprovalSnsTopic'
-        });
-        const sourceAction = new pipelineActions.GitHubSourceAction({
-            actionName: 'GitHubSource',
-            branch: githubBranch.valueAsString,
-            output: pipelineArtifact,
-            owner: githubOwner.valueAsString,
-            repo: githubRepoName.valueAsString,
-            oauthToken: sm.Secret.fromSecretName(this, 'MyGithubToken', 'my-github-token')
-                .secretValueFromJson('my-github-token'),
-            trigger: pipelineActions.GitHubTrigger.WEBHOOK,
-            runOrder: 1
-        });
-        const synthAction = SimpleSynthAction.standardNpmSynth({
-            sourceArtifact: pipelineArtifact,
-            cloudAssemblyArtifact: buildArtifact,
-            subdirectory: 'vue-web-component-app',
-            testCommands: [
-                'npm run lint',
-                'npm run test'
-            ]
-        });
-        const vuePipeline = new CdkPipeline(this, 'VueComponentCdkPipeline', {
-            pipelineName: 'VueComponentPipeline',
-            cloudAssemblyArtifact: pipelineArtifact,
-            synthAction: synthAction,
-            sourceAction: sourceAction
-        });
-
-        const approvalStage = vuePipeline.addStage('ApprovalStage');
-        approvalStage.addActions(new pipelineActions.ManualApprovalAction({
-            actionName: 'ApproveDeploy',
-            notifyEmails: [
-                emailNotifications.valueAsString
-            ],
-            additionalInformation: 'Approve Deployment to S3?',
-            externalEntityLink: `https://github.com/${githubOwner.valueAsString}/${githubRepoName.valueAsString}`,
-            notificationTopic: pipelineNotificationTopic,
-            runOrder: 1
-        }));
-        const deployStage = vuePipeline.addStage('DeployStage');
+        const deployStage = cdkPipeline.addStage('DeployStage');
         deployStage.addActions(new pipelineActions.S3DeployAction({
             actionName: 'DeployVue',
             bucket: deployBucket,
-            input: buildArtifact
+            input: vueBuildArtifact
         }));
+
     }
 }
--- cdk/test/cdk-vue-artifact.test.ts
+++ cdk/test/cdk-vue-artifact.test.ts
@@ -5,7 +5,12 @@ import * as CdkVueArtifact from '../lib/cdk-vue-artifact-stack';
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkVueArtifact.CdkVueArtifactStack(app, 'MyTestStack');
+    const stack = new CdkVueArtifact.CdkVueArtifactStack(app, 'MyTestStack', {
+        env: {
+            account: 'test',
+            region: 'us-east-1'
+        }
+    });
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
