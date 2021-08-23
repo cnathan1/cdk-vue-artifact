557dcdcf81a297ca8b9dbed49440c03a3a429a0d
butlercw <butlercw@amazon.com>
2021-08-23 10:38:38 -0400

    Add logging.

--- cdk/lib/lambda/ab-lambda-function.js
+++ cdk/lib/lambda/ab-lambda-function.js
@@ -17,66 +17,37 @@ exports.handler = (event, context, callback) => {
     // Name of cookie to check for. Application will be decided randomly when not present.
     const cookieExperimentA = 'X-Experiment-Name=A';
     const cookieExperimentB = 'X-Experiment-Name=B';
-    // Primary application version.
-    const pathExperimentA = '/index.html';
-    // Experimental application version.
-    const pathExperimentB = '/blue/index.html';
 
-    /*
-     * Lambda at the Edge headers are array objects.
-     *
-     * Client may send multiple Cookie headers, i.e.:
-     * > GET /viewerRes/test HTTP/1.1
-     * > User-Agent: curl/7.18.1 (x86_64-unknown-linux-gnu) libcurl/7.18.1 OpenSSL/1.0.1u zlib/1.2.3
-     * > Cookie: First=1; Second=2
-     * > Cookie: ClientCode=abc
-     * > Host: example.com
-     *
-     * You can access the first Cookie header at headers["cookie"][0].value
-     * and the second at headers["cookie"][1].value.
-     *
-     * Header values are not parsed. In the example above,
-     * headers["cookie"][0].value is equal to "First=1; Second=2"
-     */
-    let experimentUri;
+    // Check for cookie header to determine if experimental group has been previously selected
+    let selectedExperiment;
     if (headers.cookie) {
         for (let i = 0; i < headers.cookie.length; i++) {
             if (headers.cookie[i].value.indexOf(cookieExperimentA) >= 0) {
                 console.log('Experiment A cookie found');
-                experimentUri = pathExperimentA;
+                selectedExperiment = cookieExperimentA;
                 break;
             } else if (headers.cookie[i].value.indexOf(cookieExperimentB) >= 0) {
                 console.log('Experiment B cookie found');
-                experimentUri = pathExperimentB;
+                selectedExperiment = cookieExperimentB;
                 break;
             }
         }
     } else {
         // When there is no cookie, then randomly decide which app version will be used.
-        if (!experimentUri) {
-            console.log('Experiment cookie has not been found. Throwing dice...');
-            if (Math.random() < 0.75) {
-                experimentUri = pathExperimentA;
-                // headers.cookie = [{key: 'cookie', value: cookieExperimentA}]
-            } else {
-                experimentUri = pathExperimentB;
-                // headers.cookie = [{key: 'cookie', value: cookieExperimentB}]
-            }
+        console.log('Experiment cookie has not been found. Throwing dice...');
+        if (Math.random() < 0.75) {
+            headers.cookie = [{key: 'cookie', value: cookieExperimentA}]
+        } else {
+            headers.cookie = [{key: 'cookie', value: cookieExperimentB}]
         }
     }
 
-    if (experimentUri === pathExperimentB) {
-        //Generate HTTP redirect response to experimental group.
-        request.status = '301';
-        request.statusDescription = 'Moved Permanently';
-        headers['location'] = [{
-            key: 'Location',
-            value: pathExperimentB,
-        }];
+    if (selectedExperiment === cookieExperimentB) {
+        //Change origin to experimental group.
+        console.log('Experimental group is selected: %s', selectedExperiment);
     }
 
-    // Output the final request URI.
-    console.log(`Request uri set to "${experimentUri}"`);
+    // Output the final request.
     console.log("Final response: %j", request);
     callback(null, request);
 };
